13d2fc0be13b9413079c32fbdbd0c278b1f56994
Mateus <mateus.venancio@basis.com.br>
2023-03-23 20:24:01 -0300

    strategy jwt implementada

---
+++ src/tarefas/dto/get-user.dto.ts
@@ -0,0 +1,4 @@
+export interface GetUserDto {
+  id: string;
+  email: string;
+}
--- src/tarefas/tarefas.module.ts
+++ src/tarefas/tarefas.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { MongoModule } from 'src/mongo/mongo.module';
 import { TarefasController } from './tarefas.controller';
 import { TarefasService } from './tarefas.service';
 
 @Module({
-  imports: [MongoModule],
+  imports: [MongoModule, AuthModule],
   controllers: [TarefasController],
   providers: [TarefasService],
 })
